f5fedbc9cef2740a15564fb0e7f7807ad9e2bf9e
Motwani <amotwani@itron.com>
2024-07-14 22:51:00 -0700

    Added GitHub Actions 7-14-24-11pm

--- app/app/calc.py
+++ app/app/calc.py
@@ -4,6 +4,10 @@ Calculator functions
 
 
 def add(x, y):
-    """Ad x and y and return results."""
+    """Add x and y and return results."""
     return x + y
+
+def subtract(x, y):
+    """Subtract x and y and return results."""
+    return y - x
 
